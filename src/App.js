2dfdd7a8afd4dbf6459e70be885dd02aabe27290
walter <walteralves27@gmail.com>
2022-03-06 22:39:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [total, setTotal] = useState(0);
+  const [soma, setSoma] = useState(0);
+  const [atual, setAtual] = useState(0);
+  const [cal, setCal] = useState(0);
+
+
+  const somaPVAtual = () => {
+    let aux = cal + atual;
+    setAtual(aux);
+    console.log(aux);
+  }
+  const subtraiPVAtual = () => {
+    console.log('-');
+    //let aux += ;
+    //setTotal();
+  }
+
+  const handleChangeCalcular = (v) => {
+    setCal(v);
+  }
+
+  const handleChangeAtual = (v) => {
+    setAtual(v);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{}}>     
+      <div>
+      <label>
+        Nome: 
+        <input 
+          type="text" 
+          placeholder='Sim'
+          // value={this.state.value} 
+          // onChange={this.handleChange} 
+          />
+      </label>
+      </div>
+      
+      <div>
+      <label>
+        PV Total: 
+        <input 
+        type="text" 
+        placeholder='Total'
+        value={total} 
+        // onChange={this.handleChange} 
+        />
+      </label>
+      </div>
+      
+      <div>
+      <label>
+        PV Atual: 
+        <input 
+        type="text" 
+        placeholder='Atual'
+        value={atual} 
+        onChange={handleChangeAtual} 
+        />
+        <input 
+        type="text" 
+        placeholder='calcular'
+        value={soma} 
+        onChange={handleChangeCalcular} 
+        />
+        <button 
+          onClick={somaPVAtual}>
+            +
+        </button>
+        <button 
+          onClick={subtraiPVAtual}>
+            -
+        </button>        
+      </label>
+      </div>
+      
+      
+      
     </div>
   );
 }
