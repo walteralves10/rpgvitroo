81b8c77837efbe1c4b29bfc11e2bad2049f7277c
walteralves10 <walteralves27@gmail.com>
2022-05-27 11:13:42 -0300

    banco de producao

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const url_teste = "http://192.168.1.109:4000/rpg";
-const url_producao = "";
+//const url_teste = "http://192.168.1.109:4000/rpg";
+const url_producao = "https://rpgvitroo-backend.herokuapp.com/rpg";
 
 const api = axios.create({
-    baseURL: url_teste,
+    baseURL: url_producao,
 });
 
 export default api;
